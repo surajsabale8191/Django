de96555f8c7009b60fd7bbb2899f4fd54348b05f
surajsabale8191 <surajsabale8191@gmail.com>
2026-07-09 17:26:02 +0530

    Add templates and complete About & Contact pages
    - Added HTML templates
    - Implemented About page
    - Implemented Contact page
    - Updated routing and navigation

--- hello/Home/views.py
+++ hello/Home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from Home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index (request):
@@ -9,10 +12,22 @@ def index (request):
     #return HttpResponse("This is home page")
 
 def about(request):
-    return HttpResponse("This is about page")
+    return render(request, "about.html")
+    #return HttpResponse("This is about page")
 
 def services(request):
-    return HttpResponse(" THis is services page")
+    return render(request, "services.html")
+    #return HttpResponse(" THis is services page")
 
 def contact(request):
-    return HttpResponse(" THis is contact page")
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name, email=email, phone=phone,desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request, "Your message has been sent.")
+
+    return render(request, "contact.html")
+    #return HttpResponse(" THis is contact page")
--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Home.apps.HomeConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
